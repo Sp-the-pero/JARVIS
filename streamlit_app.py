a15ba00e7d665a8e9630f6ea39440b13ec0a86d7
Sp-the-pero <letmebegamer2@gmail.com>
2024-10-25 21:43:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 def say(Text, Speed=175, Voice="en-uk-rp3", Pitch=0):
     os.system(f'espeak "{Text}" -s {Speed} -v {Voice} -p {Pitch}')
-    print(f"JARVIS: {Text}")
+    st.write(f"JARVIS: {Text}")
     return Text
 
 def voiceInput():
@@ -16,12 +16,12 @@ def voiceInput():
             r.pause_threshold = 2 # Waits for n seconds after the user has finished speaking
             audio = r.listen(source)
             query = r.recognize_google(audio, language="en")
-            print(f"User: {query}")
+            st.write(f"User: {query}")
             return query
     except Exception as e:
         query = input("\nListening...\nUser: ")
-        print(f"The error occured: {e}")
-        # print(f"User: {query}")
+        st.write(f"The error occured: {e}")
+        # st.write(f"User: {query}")
         return query
 
 # say("Hey! I am Jarvis. An AI Copilot, that seamlessly integrates with your web browser and OS  to boost productivity with a rich communication features. What can I do for you? it could be anything like opening the browser, or giving a link.",175, "en-uk-rp 4", 0)
